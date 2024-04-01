ac7b719d872453ce72588b9bc2c41cd64a1a33d5
iliyankgeorgiev <iliyankgerogiev@gmail.com>
2024-04-01 17:54:34 +0000

    added a start script

---
+++ tools/start_backend.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+# Check if Docker Compose is already up
+if docker-compose -f ../docker-compose.yml ps | grep -q "Up"; then
+    echo "Docker Compose is already up."
+
+    # Stop the backend service
+    docker-compose -f ../docker-compose.yml stop backend
+
+    # Rebuild the backend service
+    docker-compose -f ../docker-compose.yml build backend
+
+    # Start the backend service
+    docker-compose -f ../docker-compose.yml up backend
+else
+    echo "Docker Compose is not up."
+
+    # Start the Docker Compose
+    docker-compose -f ../docker-compose.yml up --build -d
+
+    # Tail the logs of the backend service
+    docker-compose -f ../docker-compose.yml logs -f backend
+fi
+
+
+
