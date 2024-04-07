bc9d19c4b9ba82be5e3858b6f388232f46ab9bc8
iliyankg <9661240+iliyankg@users.noreply.github.com>
2024-04-07 12:49:56 +0000

    Add CollabShield context logger and update file claiming logic

---
+++ backend/server/claim.go
@@ -0,0 +1,95 @@
+package server
+
+import (
+	"context"
+	"errors"
+
+	"github.com/iliyankg/colab-shield/backend/models"
+	pb "github.com/iliyankg/colab-shield/protos"
+	"github.com/redis/go-redis/v9"
+	"github.com/rs/zerolog"
+)
+
+type MissingFileHandler func(idx int) *models.FileInfo
+type UnmarshalFailHandler func(idx int, err error) error
+
+func claimHandler(ctx context.Context, logger zerolog.Logger, redisClient *redis.Client, userId string, projectId string, request *pb.ClaimFilesRequest) (*pb.ClaimFilesResponse, error) {
+	logger.Info().Msgf("Claiming... %d files", len(request.Files))
+
+	files := make([]*models.FileInfo, 0, len(request.Files))
+	rejectedFiles := make([]*models.FileInfo, 0)
+
+	keys := make([]string, 0, len(request.Files))
+	keysFromFileRequests(projectId, request.Files, &keys)
+
+	// Handler for missing files in the Redis hash
+	missingFileHandler := func(idx int) *models.FileInfo {
+		return models.NewFileInfo(request.Files[idx].FileId, request.Files[idx].FileHash, request.BranchName)
+	}
+
+	// Handler for failed unmarshalling of JSON from the Redis hash
+	unmarshalFailHandler := func(idx int, err error) error {
+		logger.Error().Str("key", keys[idx]).Err(err).Msg("Failed to unmarshal JSON from Redis hash")
+		return nil
+	}
+
+	// Watch function to ensure keys do not get modified by another request while this transaction
+	// is in progress
+	watchFn := func(tx *redis.Tx) error {
+		result, err := tx.JSONMGet(ctx, "$", keys...).Result()
+		if err != nil {
+			logger.Error().Err(err).Msg("Failed to read keys from Redis hash")
+			return err
+		}
+
+		err = parseFileInfos(result, &files, missingFileHandler, unmarshalFailHandler)
+		if err != nil {
+			logger.Error().Err(err).Msg("Failed to parse file infos from Redis hash")
+			return err
+		}
+
+		claimFiles(userId, files, request.Files, &rejectedFiles)
+
+		if len(rejectedFiles) > 0 {
+			return ErrRejectedFiles
+		}
+
+		return setFiles(ctx, logger, redisClient, keys, files)
+	}
+
+	// Execute the watch function
+	err := redisClient.Watch(ctx, watchFn, keys...)
+
+	if errors.Is(err, ErrRejectedFiles) {
+		protoRejectedFiles := make([]*pb.FileInfo, 0, len(rejectedFiles))
+		models.FileInfosToProto(rejectedFiles, &protoRejectedFiles)
+
+		return &pb.ClaimFilesResponse{
+			Status:        pb.Status_ERROR,
+			RejectedFiles: protoRejectedFiles,
+		}, nil
+	} else if err != nil {
+		logger.Error().Err(err).Msg("Failed to claim files")
+		return nil, err
+	}
+
+	logger.Info().Msg("Claiming successful")
+
+	// TODO: Consider returning the files that were claimed succesfully
+	return &pb.ClaimFilesResponse{
+		Status: pb.Status_OK,
+	}, nil
+}
+
+func claimFiles(userId string, fileInfos []*models.FileInfo, claimRequests []*pb.ClaimFileInfo, outRejectedFiles *[]*models.FileInfo) {
+	for i := range fileInfos {
+		reqFile := claimRequests[i]
+
+		// try claiming the file
+		if err := fileInfos[i].Claim(userId, reqFile.FileHash, reqFile.ClaimMode); err != nil {
+			// we do not return the error imediately so we can build a full list of rejected files
+			// and report them back all at once
+			*outRejectedFiles = append(*outRejectedFiles, fileInfos[i])
+		}
+	}
+}
---
+++ backend/server/common.go
@@ -0,0 +1,62 @@
+package server
+
+import (
+	"context"
+	"encoding/json"
+	"fmt"
+
+	"github.com/iliyankg/colab-shield/backend/models"
+	"github.com/redis/go-redis/v9"
+	"github.com/rs/zerolog"
+)
+
+// parseFileInfos parses the file infos from the Redis hash and creates new ones where appropriate.
+func parseFileInfos(toParse []any, outFileInfos *[]*models.FileInfo, missingFileHandler MissingFileHandler, unmarshalFailHandler UnmarshalFailHandler) error {
+	// parse all files from the Redis and create new where appropriate
+	for i, res := range toParse {
+		// key does not exist in the DB so assume brand new
+		if res == nil {
+			if missingFileHandler != nil {
+				*outFileInfos = append(*outFileInfos, missingFileHandler(i))
+			} else {
+				*outFileInfos = append(*outFileInfos, nil)
+			}
+			continue
+		}
+
+		// unmarshal the JSON from the Redis hash
+		var fileInfo models.FileInfo
+		if err := json.Unmarshal([]byte(res.(string)), &fileInfo); err != nil {
+			if unmarshalFailHandler == nil {
+				continue
+			}
+
+			err = unmarshalFailHandler(i, err)
+			if err != nil {
+				return err
+			}
+		}
+
+		*outFileInfos = append(*outFileInfos, &fileInfo)
+	}
+
+	return nil
+}
+
+func setFiles(ctx context.Context, logger zerolog.Logger, redisClient *redis.Client, keys []string, fileInfos []*models.FileInfo) error {
+	// build the mset params
+	mSetParams := make([]any, 0, len(fileInfos)*3)
+	for i, file := range fileInfos {
+		mSetParams = append(mSetParams, keys[i], "$", *file)
+	}
+
+	logger.Debug().Str("params", fmt.Sprintf("%v", mSetParams)).Msgf("Invoking JSONMSet")
+	// Not pipelined because it uses MULTI/EXEC internally already and redis does not support
+	// nested transactions.
+	if err := redisClient.JSONMSet(ctx, mSetParams...).Err(); err != nil {
+		logger.Error().Err(err).Msg("Failed to write to Redis hash")
+		return err
+	}
+
+	return nil
+}
