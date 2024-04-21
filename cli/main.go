ccc02a86fa1654f6bd991cd5f3c73d8dc75f95ea
iliyankg <9661240+iliyankg@users.noreply.github.com>
2024-04-21 10:40:19 +0000

    feat: file listing to cli

--- cli/main.go
+++ cli/main.go
@@ -2,11 +2,8 @@ package main
 
 import (
 	"github.com/iliyankg/colab-shield/cli/cmd"
-	"github.com/rs/zerolog"
 )
 
 func main() {
-	zerolog.TimeFieldFormat = zerolog.TimeFormatUnix
-
 	cmd.Execute()
 }
