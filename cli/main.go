94558fba7eb4f4b30320e3519173e9ca91db20a6
iliyankg <9661240+iliyankg@users.noreply.github.com>
2024-04-23 19:03:44 +0000

    added viper to project

--- cli/main.go
+++ cli/main.go
@@ -1,9 +1,20 @@
 package main
 
 import (
+	"github.com/rs/zerolog/log"
+
 	"github.com/iliyankg/colab-shield/cli/cmd"
+	"github.com/spf13/viper"
 )
 
 func main() {
+	viper.SetConfigName("config.json")
+	viper.AddConfigPath(".")
+
+	err := viper.ReadInConfig()
+	if err != nil {
+		log.Fatal().Err(err).Msg("Failed to read config from working directory. Make sure config.json is present.")
+	}
+
 	cmd.Execute()
 }
