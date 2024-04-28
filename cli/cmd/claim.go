86bee145b646ddf98f895e39dc048cae5fc67522
iliyankg <9661240+iliyankg@users.noreply.github.com>
2024-04-28 17:59:14 +0000

    test: add tests for common cmd functions

--- cli/cmd/claim.go
+++ cli/cmd/claim.go
@@ -69,6 +69,30 @@ var claimFilesCmd = &cobra.Command{
 	},
 }
 
+// newClaimFilesRequest creates a new ClaimFilesRequest from the given files and hashes
+// Same claim mode is applied to all files.
+// TODO: Per file claim mode support?
+func newClaimFilesRequest(files []string, hashes []string, claimMode protos.ClaimMode, softClaim bool) (*protos.ClaimFilesRequest, error) {
+	if len(files) != len(hashes) {
+		return nil, ErrFileToHashMissmatch
+	}
+
+	claimFileInfos := make([]*protos.ClaimFileInfo, 0, len(files))
+	for i, file := range files {
+		claimFileInfos = append(claimFileInfos, &protos.ClaimFileInfo{
+			FileId:    file,
+			FileHash:  hashes[i],
+			ClaimMode: claimMode,
+		})
+	}
+
+	return &protos.ClaimFilesRequest{
+		BranchName: gitBranch,
+		Files:      claimFileInfos,
+		SoftClaim:  softClaim,
+	}, nil
+}
+
 // validateClaimMode validates the incoming integer is valid.
 // FIXME: This ties is to protobuf and not ideal.
 func validateClaimMode(mode int32) bool {
--- cli/cmd/common.go
+++ cli/cmd/common.go
@@ -7,7 +7,6 @@ import (
 	"time"
 
 	"github.com/iliyankg/colab-shield/cli/config"
-	"github.com/iliyankg/colab-shield/protos"
 	"google.golang.org/grpc/metadata"
 )
 
@@ -24,30 +23,6 @@ func buildContext(projectId string, userId string) (context.Context, context.Can
 	)
 
 	return metadata.NewOutgoingContext(ctx, metaInfo), cancel
-}
-
-// newClaimFilesRequest creates a new ClaimFilesRequest from the given files and hashes
-// Same claim mode is applied to all files.
-// TODO: Per file claim mode support?
-func newClaimFilesRequest(files []string, hashes []string, claimMode protos.ClaimMode, softClaim bool) (*protos.ClaimFilesRequest, error) {
-	if len(files) != len(hashes) {
-		return nil, ErrFileToHashMissmatch
-	}
-
-	claimFileInfos := make([]*protos.ClaimFileInfo, 0, len(files))
-	for i, file := range files {
-		claimFileInfos = append(claimFileInfos, &protos.ClaimFileInfo{
-			FileId:    file,
-			FileHash:  hashes[i],
-			ClaimMode: claimMode,
-		})
-	}
-
-	return &protos.ClaimFilesRequest{
-		BranchName: gitBranch,
-		Files:      claimFileInfos,
-		SoftClaim:  softClaim,
-	}, nil
 }
 
 // filterToFilesOfInterest filters the given files to only those that are of interest basedon
