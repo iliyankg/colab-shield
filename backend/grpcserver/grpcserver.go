4761778e9bf4456ec131d166ba17b68bef132a53
iliyankg <9661240+iliyankg@users.noreply.github.com>
2024-05-08 19:19:20 +0000

    refactor: clean up and consolidating of logic and files

--- backend/grpcserver/grpcserver.go
+++ backend/grpcserver/grpcserver.go
@@ -2,9 +2,13 @@ package grpcserver
 
 import (
 	"context"
+	"encoding/json"
+	"errors"
 	"fmt"
 	"net"
 
+	"github.com/iliyankg/colab-shield/backend/core"
+	"github.com/iliyankg/colab-shield/backend/core/requests"
 	"github.com/iliyankg/colab-shield/protos"
 	"github.com/redis/go-redis/v9"
 	"github.com/rs/zerolog"
@@ -70,25 +74,106 @@ func (s *ColabShieldServer) ListFiles(ctx context.Context, request *protos.ListF
 		With().
 		Logger()
 
-	return listHandler(ctx, logger, s.redisClient, userIdFromCtx(ctx), projectIdFromCtx(ctx), request)
+	projectId := projectIdFromCtx(ctx)
+
+	files, cursor, err := core.List(ctx, logger, s.redisClient, projectId, request.Cursor, request.PageSize, request.FolderPath)
+	if err != nil {
+		return nil, parseCoreError(err)
+	}
+
+	protoFiles := make([]*protos.FileInfo, 0, len(files))
+	fileInfosToProto(files, &protoFiles)
+	return &protos.ListFilesResponse{
+		NextCursor: cursor,
+		Files:      protoFiles,
+	}, nil
 }
 
-func (s *ColabShieldServer) Claim(ctx context.Context, request *protos.ClaimFilesRequest) (*protos.ClaimFilesResponse, error) {
+func (s *ColabShieldServer) Claim(ctx context.Context, req *protos.ClaimFilesRequest) (*protos.ClaimFilesResponse, error) {
 	logger := zerolog.Ctx(ctx).
 		With().
-		Str("branchName", request.BranchName).
+		Str("branchName", req.BranchName).
 		Logger()
 
-	return claimHandler(ctx, logger, s.redisClient, userIdFromCtx(ctx), projectIdFromCtx(ctx), request)
+	userId := userIdFromCtx(ctx)
+	projectId := projectIdFromCtx(ctx)
+
+	res, err := json.Marshal(req)
+	if err != nil {
+		logger.Error().Err(err).Msg("Failed to marshal JSON from Redis hash")
+		return nil, ErrMarshalFail
+	}
+
+	var internalReq requests.Claim
+	if err := json.Unmarshal(res, &internalReq); err != nil {
+		logger.Error().Err(err).Msg("Failed to unmarshal JSON from Redis hash")
+		return nil, ErrUnmarshalFail
+	}
+
+	rejectedFiles, err := core.Claim(ctx, logger, s.redisClient, userId, projectId, &internalReq)
+	parsedErr := parseCoreError(err)
+	if errors.Is(parsedErr, ErrRejectedFiles) {
+		logger.Info().Msg("Claiming failed due to rejected files")
+		protoRejectedFiles := make([]*protos.FileInfo, 0, len(rejectedFiles))
+		fileInfosToProto(rejectedFiles, &protoRejectedFiles)
+
+		return &protos.ClaimFilesResponse{
+			Status:        protos.Status_REJECTED,
+			RejectedFiles: protoRejectedFiles,
+		}, nil
+	} else if parsedErr != nil {
+		logger.Error().Err(parsedErr).Msg("Failed to claim files")
+		return nil, parsedErr
+	}
+
+	// TODO: Consider returning the files that were claimed succesfully
+	return &protos.ClaimFilesResponse{
+		Status: protos.Status_OK,
+	}, nil
 }
 
-func (s *ColabShieldServer) Update(ctx context.Context, request *protos.UpdateFilesRequest) (*protos.UpdateFilesResponse, error) {
+func (s *ColabShieldServer) Update(ctx context.Context, req *protos.UpdateFilesRequest) (*protos.UpdateFilesResponse, error) {
 	logger := zerolog.Ctx(ctx).
 		With().
-		Str("branchName", request.BranchName).
+		Str("branchName", req.BranchName).
 		Logger()
 
-	return updateHandler(ctx, logger, s.redisClient, userIdFromCtx(ctx), projectIdFromCtx(ctx), request)
+	userId := userIdFromCtx(ctx)
+	projectId := projectIdFromCtx(ctx)
+
+	res, err := json.Marshal(req)
+	if err != nil {
+		logger.Error().Err(err).Msg("Failed to marshal JSON from Redis hash")
+		return nil, ErrMarshalFail
+	}
+
+	var internalReq requests.Update
+	if err := json.Unmarshal(res, &internalReq); err != nil {
+		logger.Error().Err(err).Msg("Failed to unmarshal JSON from Redis hash")
+		return nil, ErrUnmarshalFail
+	}
+
+	rejectedFiles, err := core.Update(ctx, logger, s.redisClient, userId, projectId, &internalReq)
+	parsedErr := parseCoreError(err)
+	if errors.Is(parsedErr, ErrRejectedFiles) {
+		logger.Info().Msg("Updating failed due to rejected files")
+		protoRejectedFiles := make([]*protos.FileInfo, 0, len(rejectedFiles))
+		fileInfosToProto(rejectedFiles, &protoRejectedFiles)
+
+		return &protos.UpdateFilesResponse{
+			Status:        protos.Status_REJECTED,
+			RejectedFiles: protoRejectedFiles,
+		}, nil
+	} else if parsedErr != nil {
+		logger.Error().Err(parsedErr).Msg("Failed to update files")
+		return nil, parsedErr
+	}
+
+	logger.Info().Msg("Updating successful")
+
+	return &protos.UpdateFilesResponse{
+		Status: protos.Status_OK,
+	}, nil
 }
 
 func (s *ColabShieldServer) Release(ctx context.Context, request *protos.ReleaseFilesRequest) (*protos.ReleaseFilesResponse, error) {
@@ -96,5 +181,28 @@ func (s *ColabShieldServer) Release(ctx context.Context, request *protos.Release
 		With().
 		Logger()
 
-	return releaseHandler(ctx, logger, s.redisClient, userIdFromCtx(ctx), projectIdFromCtx(ctx), request)
+	userId := userIdFromCtx(ctx)
+	projectId := projectIdFromCtx(ctx)
+
+	rejectedFiles, err := core.Release(ctx, logger, s.redisClient, userId, projectId, request.BranchName, request.FileIds)
+	parsedErr := parseCoreError(err)
+	if errors.Is(parsedErr, ErrRejectedFiles) {
+		logger.Info().Msg("Releasing failed due to rejected files")
+		protoRejectedFiles := make([]*protos.FileInfo, 0, len(rejectedFiles))
+		fileInfosToProto(rejectedFiles, &protoRejectedFiles)
+
+		return &protos.ReleaseFilesResponse{
+			Status:        protos.Status_REJECTED,
+			RejectedFiles: protoRejectedFiles,
+		}, nil
+	} else if parsedErr != nil {
+		logger.Error().Err(parsedErr).Msg("Failed to release files")
+		return nil, parsedErr
+	}
+
+	logger.Info().Msg("Releasing successful")
+
+	return &protos.ReleaseFilesResponse{
+		Status: protos.Status_OK,
+	}, nil
 }
