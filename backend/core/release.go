0657d624d5480ed46aec0c8081a8379c23786d8f
iliyankg <9661240+iliyankg@users.noreply.github.com>
2024-05-08 18:35:25 +0000

    refactor: move release to core

---
+++ backend/core/release.go
@@ -0,0 +1,82 @@
+package core
+
+import (
+	"context"
+	"errors"
+
+	"github.com/iliyankg/colab-shield/backend/models"
+	"github.com/redis/go-redis/v9"
+	"github.com/rs/zerolog"
+)
+
+func Release(ctx context.Context, logger zerolog.Logger, rc *redis.Client, userId string, projectId string, branchId string, fileIds []string) ([]*models.FileInfo, error) {
+	if len(fileIds) == 0 {
+		logger.Warn().Msg("No files to release")
+		return nil, nil
+	}
+
+	logger.Info().Msgf("Releasing... %d files", len(fileIds))
+
+	files := make([]*models.FileInfo, 0, len(fileIds))
+	rejectedFiles := make([]*models.FileInfo, 0)
+
+	keys := make([]string, 0, len(fileIds))
+	for _, fileId := range fileIds {
+		keys = append(keys, buildRedisKeyForFile(projectId, fileId))
+	}
+
+	// Handler for missing files in the Redis hash
+	missingFileHandler := func(idx int) *models.FileInfo {
+		rejectedFiles = append(rejectedFiles, models.NewMissingFileInfo(fileIds[idx]))
+		return nil
+	}
+
+	// Watch function to ensure keys do not get modified by another request while this transaction
+	// is in progress
+	watchFn := func(tx *redis.Tx) error {
+		if err := getFileInfos(ctx, logger, rc, keys, missingFileHandler, &files); err != nil {
+			return err
+		}
+
+		if len(rejectedFiles) > 0 {
+			return ErrRejectedFiles
+		}
+
+		releaseFiles(userId, files, &rejectedFiles)
+
+		if len(rejectedFiles) > 0 {
+			return ErrRejectedFiles
+		}
+
+		return setFileInfos(ctx, logger, tx, keys, files)
+	}
+
+	// Execute the watch function
+	err := rc.Watch(ctx, watchFn, keys...)
+
+	if errors.Is(err, ErrRejectedFiles) {
+		logger.Info().Msg("Releasing failed due to rejected files")
+		return rejectedFiles, nil
+	} else if err != nil {
+		logger.Error().Err(err).Msg("Failed to release files")
+		return nil, err
+	}
+
+	logger.Info().Msg("Releasing successful")
+	return nil, nil
+}
+
+func releaseFiles(userId string, fileInfos []*models.FileInfo, outRejectedFiles *[]*models.FileInfo) {
+	// update the files with the new file hashes
+	for i := range fileInfos {
+		if fileInfos[i] == nil {
+			continue
+		}
+
+		if err := fileInfos[i].Release(userId); err != nil {
+			// we do not return the error imediately so we can build a full list of rejected files
+			// and report them back all at once
+			*outRejectedFiles = append(*outRejectedFiles, fileInfos[i])
+		}
+	}
+}
--- backend/grpcserver/release.go
+++ backend/grpcserver/release.go
@@ -4,62 +4,16 @@ import (
 	"context"
 	"errors"
 
-	"github.com/iliyankg/colab-shield/backend/colabom"
-	"github.com/iliyankg/colab-shield/backend/models"
+	"github.com/iliyankg/colab-shield/backend/core"
 	"github.com/iliyankg/colab-shield/protos"
 	"github.com/redis/go-redis/v9"
 	"github.com/rs/zerolog"
 )
 
 func releaseHandler(ctx context.Context, logger zerolog.Logger, rc *redis.Client, userId string, projectId string, request *protos.ReleaseFilesRequest) (*protos.ReleaseFilesResponse, error) {
-	if len(request.FileIds) == 0 {
-		logger.Warn().Msg("No files to release")
-		// TODO: Consider returning an error here
-		return &protos.ReleaseFilesResponse{
-			Status: protos.Status_OK,
-		}, nil
-	}
-
-	logger.Info().Msgf("Releasing... %d files", len(request.FileIds))
-
-	files := make([]*models.FileInfo, 0, len(request.FileIds))
-	rejectedFiles := make([]*models.FileInfo, 0)
-
-	keys := make([]string, 0, len(request.FileIds))
-	for _, fileId := range request.FileIds {
-		keys = append(keys, colabom.BuildRedisKeyForFile(projectId, fileId))
-	}
-
-	// Handler for missing files in the Redis hash
-	missingFileHandler := func(idx int) *models.FileInfo {
-		rejectedFiles = append(rejectedFiles, models.NewMissingFileInfo(request.FileIds[idx]))
-		return nil
-	}
-
-	// Watch function to ensure keys do not get modified by another request while this transaction
-	// is in progress
-	watchFn := func(tx *redis.Tx) error {
-		if err := colabom.GetFileInfos(ctx, logger, rc, keys, missingFileHandler, &files); err != nil {
-			return parseCoreError(err)
-		}
-
-		if len(rejectedFiles) > 0 {
-			return ErrRejectedFiles
-		}
-
-		releaseFiles(userId, files, &rejectedFiles)
-
-		if len(rejectedFiles) > 0 {
-			return ErrRejectedFiles
-		}
-
-		return parseCoreError(colabom.SetFileInfos(ctx, logger, tx, keys, files))
-	}
-
-	// Execute the watch function
-	err := rc.Watch(ctx, watchFn, keys...)
-
-	if errors.Is(err, ErrRejectedFiles) {
+	rejectedFiles, err := core.Release(ctx, logger, rc, userId, projectId, request.BranchName, request.FileIds)
+	parsedErr := parseCoreError(err)
+	if errors.Is(parsedErr, ErrRejectedFiles) {
 		logger.Info().Msg("Releasing failed due to rejected files")
 		protoRejectedFiles := make([]*protos.FileInfo, 0, len(rejectedFiles))
 		fileInfosToProto(rejectedFiles, &protoRejectedFiles)
@@ -68,9 +22,9 @@ func releaseHandler(ctx context.Context, logger zerolog.Logger, rc *redis.Client
 			Status:        protos.Status_REJECTED,
 			RejectedFiles: protoRejectedFiles,
 		}, nil
-	} else if err != nil {
-		logger.Error().Err(err).Msg("Failed to release files")
-		return nil, err
+	} else if parsedErr != nil {
+		logger.Error().Err(parsedErr).Msg("Failed to release files")
+		return nil, parsedErr
 	}
 
 	logger.Info().Msg("Releasing successful")
@@ -78,19 +32,4 @@ func releaseHandler(ctx context.Context, logger zerolog.Logger, rc *redis.Client
 	return &protos.ReleaseFilesResponse{
 		Status: protos.Status_OK,
 	}, nil
-}
-
-func releaseFiles(userId string, fileInfos []*models.FileInfo, outRejectedFiles *[]*models.FileInfo) {
-	// update the files with the new file hashes
-	for i := range fileInfos {
-		if fileInfos[i] == nil {
-			continue
-		}
-
-		if err := fileInfos[i].Release(userId); err != nil {
-			// we do not return the error imediately so we can build a full list of rejected files
-			// and report them back all at once
-			*outRejectedFiles = append(*outRejectedFiles, fileInfos[i])
-		}
-	}
 }
