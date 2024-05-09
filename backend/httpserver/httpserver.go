613cf2bf4ded76d0ae4972d624c6bd08c33238cd
iliyankg <9661240+iliyankg@users.noreply.github.com>
2024-05-09 19:39:06 +0000

    refactor: cleanup on handlers and handler creation

--- backend/httpserver/httpserver.go
+++ backend/httpserver/httpserver.go
@@ -1,14 +1,19 @@
 package httpserver
 
 import (
+	"context"
 	"fmt"
+	"time"
 
+	"github.com/alexliesenfeld/health"
 	"github.com/gin-contrib/requestid"
 	"github.com/gin-gonic/gin"
 	"github.com/rs/zerolog/log"
 
 	"github.com/redis/go-redis/v9"
 )
+
+const version = "0.1.0" // TODO: Get this from the build system
 
 // ColabShieldServer is the main HTTP server for Colab Shield
 type ColabShieldServer struct {
@@ -19,11 +24,8 @@ type ColabShieldServer struct {
 func NewColabShieldServer(redisClient *redis.Client) *ColabShieldServer {
 	ginEngine := gin.New()
 	ginEngine.Use(requestid.New())
-	ginEngine.Use(newGinLoggingHandler())
+	ginEngine.Use(createGinLoggingHandler())
 	ginEngine.Use(gin.Recovery())
-
-	ginEngine.GET("/health", newGinHealthHandler(redisClient))
-	ginEngine.POST("/files/claim")
 
 	return &ColabShieldServer{
 		ginEngine,
@@ -32,6 +34,33 @@ func NewColabShieldServer(redisClient *redis.Client) *ColabShieldServer {
 }
 
 func (css *ColabShieldServer) Serve(port int) error {
+	css.ginEngine.GET("/health", css.createHealthHandler())
+	css.ginEngine.POST("/files/claim", css.claimHandler)
+
 	log.Info().Msgf("Http listening on port: %d", port)
 	return css.ginEngine.Run(fmt.Sprintf("0.0.0.0:%d", port))
+}
+
+func (css *ColabShieldServer) claimHandler(ctx *gin.Context) {
+	// TODO: Implement
+}
+
+func (css *ColabShieldServer) createHealthHandler() gin.HandlerFunc {
+	checker := health.NewChecker(
+		health.WithInfo(map[string]any{
+			"version": version,
+		}),
+		health.WithCheck(health.Check{
+			Name:    "redis",
+			Timeout: 3 * time.Second,
+			Check: func(ctx context.Context) error {
+				return css.redisClient.Ping(ctx).Err()
+			},
+		}),
+	)
+
+	return func(c *gin.Context) {
+		httpHandler := health.NewHandler(checker)
+		httpHandler(c.Writer, c.Request)
+	}
 }
--- backend/httpserver/logging.go
+++ backend/httpserver/logging.go
@@ -13,9 +13,9 @@ func getLogger(c *gin.Context) *zerolog.Logger {
 	return c.MustGet(LoggerKeyCtxKey).(*zerolog.Logger)
 }
 
-// newGinLoggingHandler returns a new gin middleware that logs requests.
+// createGinLoggingHandler returns a new gin middleware that logs requests.
 // Also adds a logger to the context for use in other handlers.
-func newGinLoggingHandler() gin.HandlerFunc {
+func createGinLoggingHandler() gin.HandlerFunc {
 	loggerOption := logger.WithLogger(func(c *gin.Context, logger zerolog.Logger) zerolog.Logger {
 		reqUUID := c.Request.Header.Get("X-Request-ID")
 		updatedLogger := logger.Output(gin.DefaultWriter).With().Str("uuid", reqUUID).Logger()
